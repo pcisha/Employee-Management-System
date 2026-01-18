fbfdd4eb809916a8a43bff1694fcdc4edbd090f8
Prachi S. (＾◡＾)っ ♡ <prachi.shah.pro@gmail.com>
2026-01-17 20:58:12 -0800

    Update db_conn_mysqli.php

--- service/db_conn_mysqli.php
+++ service/db_conn_mysqli.php
@@ -1,8 +1,9 @@
 <?php
-$con=mysqli_connect("silo.cs.indiana.edu","b561f13_amlakhan","password","b561f13_amlakhan");
+$con=mysqli_connect("silo.cs.indiana.edu","abc_username","password","abc_password");
 // Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
+
 ?>
